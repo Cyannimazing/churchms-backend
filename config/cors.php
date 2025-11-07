9c3b786647ae9d4ada771feff0f9091b67d0ce4b
Klir Balista <klirbalista@gmail.com>
2025-11-07 11:02:36 +0800

    Add CORS pattern to match Vercel deployments

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+    ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '/^https:\/\/church-ms-.*\.vercel\.app$/',
+    ],
 
     'allowed_headers' => ['*'],
 
